97d3454722e820d0f879a5d38055498dc9657689
easychessanimations <chessbotroulette@gmail.com>
2020-04-16 16:03:52 +0200

    Install script

--- s/install.bat
+++ s/install.bat
@@ -10,5 +10,9 @@ set GOARCH=amd64
 
 go install uciengine/zurimain.go
 
-copy zurimain.exe %EASYCHESS_PATH%\resources\server\bin\gochess.exe
-copy zurimain %EASYCHESS_PATH%\resources\server\bin\gochess
+copy zurimain.exe %EASYCHESS_PATH%\resources\server\bin\zurimain_upload.exe
+copy zurimain %EASYCHESS_PATH%\resources\server\bin\zurimain_upload
+
+cd %EASYCHESS_PATH%
+
+s\uploadengine
